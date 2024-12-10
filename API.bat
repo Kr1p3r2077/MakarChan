afb0c2c06ef5385fb005b59f51a87b8df6d66148
Сметанка <62791234+Kr1p3r2077@users.noreply.github.com>
2024-12-11 00:34:11 +0300

    Delete API.bat

--- API.bat
+++
@@ -1,8 +0,0 @@
-:: Возвращаемся в корневую директорию
-cd /d F:\Pycharm\Makarchan
-
-:: Активируем виртуальное окружение
-call .venv\scripts\activate.bat
-
-:: Запустим второй экземпляр uvicorn
-start uvicorn main:app --host 0.0.0.0 --port 8888
